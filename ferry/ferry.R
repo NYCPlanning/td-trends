3a62376423481011ececa9fd59ef233693e34479
Yijun Ma <mayijun1203@gmail.com>
2021-11-30 11:13:01 -0500

    update

--- ferry/ferry.R
+++ ferry/ferry.R
@@ -2,20 +2,20 @@ library(tidyverse)
 library(plotly)
 
 path='C:/Users/Y_Ma2/Desktop/GITHUB/td-trends/'
-path='C:/Users/mayij/Desktop/DOC/GITHUB/td-trends/'
+# path='C:/Users/mayij/Desktop/DOC/GITHUB/td-trends/'
 
 df=read.csv(paste0(path,'ferry/ferryop.csv'),stringsAsFactors=F,check.names=F)
 df=df %>%
   mutate(Date=as.Date(paste0(yearmonth,'01'),'%Y%m%d')) %>%
   filter(Date>=as.Date('20150101','%Y%m%d'))
 
-dfcolors=c('BillyBey'='rgba(255,158,74,1)',
-           'Liberty Landing Ferry'='rgba(103,191,92,1)',
-           'NY Waterway'='rgba(173,139,201,1)',
-           'NYC Ferry'='rgba(237,102,93,1)',
-           'New York Water Taxi'='rgba(205,204,93,1)',
-           'SeaStreak'='rgba(237,151,202,1)',
-           'Staten Island Ferry'='rgba(114,158,206,1')
+dfcolors=c('BillyBey'='rgba(255,158,74,0.8)',
+           'Liberty Landing Ferry'='rgba(103,191,92,0.8)',
+           'NY Waterway'='rgba(173,139,201,0.8)',
+           'NYC Ferry'='rgba(237,102,93,0.8)',
+           'New York Water Taxi'='rgba(205,204,93,0.8)',
+           'SeaStreak'='rgba(237,151,202,0.8)',
+           'Staten Island Ferry'='rgba(114,158,206,0.8')
 
 p=plot_ly()
 for (i in list('Staten Island Ferry','BillyBey','Liberty Landing Ferry','NY Waterway','New York Water Taxi','SeaStreak','NYC Ferry')){
@@ -83,12 +83,9 @@ p=p %>%
                           y=-0.2,
                           yanchor='top',
                           yref='paper'))
+p=p %>%
+  config(displayModeBar=F)
 p
-
-# Remove camera and logo
-
-
-
 htmlwidgets::saveWidget(p,paste0(path,'ferry/ferry.html'))
 
 
