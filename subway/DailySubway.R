4d647b0799cc9c19f9703ddce4cdc8acc6cc1226
ssanichar <93738034+ssanichar@users.noreply.github.com>
2021-12-03 09:29:42 -0500

    Update DailySubway.R

--- subway/DailySubway.R
+++ subway/DailySubway.R
@@ -15,7 +15,7 @@ p=p %>%
   add_trace(type="scatter", mode="lines", x=df[["Date"]], y=df[["Subways..Total.Estimated.Ridership"]], hovertemplate="%{y:,.0f}") %>%
   layout(title="Daily Subway Ridership During COVID-19", xaxis=list(title="Date"), yaxis=list(title="Ridership")) %>%
   layout(showlegend=FALSE)%>%
-  layout(margin = list(b=160), annotations=list(x=1, y=-0.2, text= "Data Source: MTA", showarrow=F, xref="paper", yref="paper", xanchor="right", yanchor="top", xshift=0, yshift=0, font=list(size=12, color="grey"))) %>%
+  layout(margin = list(b=160), annotations=list(x=1, y=-0.1, text= "Data Source: MTA", showarrow=F, xref="paper", yref="paper", xanchor="right", yanchor="top", xshift=0, yshift=0, font=list(size=12, color="grey"))) %>%
   config(displayModeBar = F)
 
 p
