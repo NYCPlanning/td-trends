8e111c6f483161de961373d516da4492d2558a6c
mariafree <93666624+mariafree@users.noreply.github.com>
2022-01-26 13:56:40 -0500

    Update hubbound_all.py

--- hubbound/all_modes/hubbound_all.py
+++ hubbound/all_modes/hubbound_all.py
@@ -16,28 +16,37 @@ pio.renderers.default = 'browser'
 # path = '/Users/Work/Desktop/GitHub/td-trends/hubbound/all_modes/'
 path = 'C:/Users/M_Free/Desktop/td-trends/hubbound/all_modes/'
 
-df = pd.read_csv(path + 'hubbound_all.csv', index_col = [0,1])
+df = pd.read_csv(path + 'hubbound_all.csv')
+df['hover'] = '<i>' + df['mode'] + ': </i>' + df['entries'].map('{:,.0f}'.format) + ' (' + df['%'].map('{:.0%}'.format) + ')'
 
-mode_colors = {'Auto': '#ed665d',
-               'Bus': '#67bf5c',
-               'Subway': '#729ece',
+df_total = df[['year', 'entries']].groupby('year').sum().reset_index()
+df_total['year'] = df_total['year'].astype(str)
+
+mode_colors = {'Subway': '#729ece',
+               'Auto': '#ed665d',
                'Rail': '#ff9e4a',
+               'Bus': '#67bf5c',
                'Other': '#ed97ca'}
-
-# ENTRIES BY MODE
-
-df_entries = df.loc[slice(None), 'Entries', :].reset_index().drop('Type', axis = 1)
 
 fig = go.Figure()
 
-for mode, color in mode_colors.items(): 
-    fig = fig.add_trace(go.Scattergl(name = mode,
-                                     mode = 'lines+markers',
-                                     x = df_entries['Year'],
-                                     y = df_entries[mode],
-                                     line = {'color': color,
-                                             'width': 2},
-                                     hovertemplate = '%{y:,.0f}'))
+for mode, color in mode_colors.items():
+    fig = fig.add_trace(go.Scatter(name = mode,
+                                   x = df.loc[df['mode'] == mode, 'year'],
+                                   y = df.loc[df['mode'] == mode, 'entries'],
+                                   mode = 'lines',
+                                   stackgroup = 'one',
+                                   line = {'color': color,
+                                           'width': 2},
+                                   hoverinfo = 'text',
+                                   hovertext = df.loc[df['mode'] == mode, 'hover']))
+
+fig = fig.add_trace(go.Scatter(x = df_total['year'],
+                               y = df_total['entries'],
+                               mode = 'none',
+                               showlegend = False,
+                               hoverinfo = 'text',
+                               hovertext = '<b>' + df_total['year'] + ' <br>Total Entries: ' + df_total['entries'].map('{:,.0f}'.format)))
 
 fig.update_layout(template = 'plotly_white',
                   title = {'text': '<b>Daily Entries into the Manhattan Hub</b>',
@@ -61,7 +70,7 @@ fig.update_layout(template = 'plotly_white',
                                      'font_size': 14},
                            'tickfont_size': 12,
                            'dtick': 'M12',
-                           'range': [min(df_entries['Year'])-0.5, max(df_entries['Year'])+0.5],
+                           'range': [min(df['year'])-0.5, max(df['year'])+0.5],
                            'fixedrange': True,
                            'showgrid': False},
                   yaxis = {'title':{'text': '<b>Daily Entries</b>',
@@ -88,82 +97,8 @@ fig.add_annotation(text = 'Data Source: <a href="https://www.nymtc.org/Data-and-
 
 fig
 
-# fig.write_html(path + 'annotations/commuters.html',
-#                 include_plotlyjs = 'cdn',
-#                 config = {'displayModeBar': False})
+fig.write_html(path + 'annotations/entries.html',
+              include_plotlyjs='cdn',
+              config={'displayModeBar':False})
 
-print('Chart Available at: https://nycplanning.github.io/td-trends/hubbound/all_modes/annotations/entries.html')  
-
-# MODE SPLIT
-
-df_pct = df.loc[slice(None), '%', :].reset_index().drop('Type', axis = 1)
-
-fig = go.Figure()
-
-for mode, color in mode_colors.items(): 
-    
-    
-    fig.add_trace(go.Scatter(name = mode,
-                             x = df_pct['Year'],
-                             y = df_pct[mode],
-                             mode = 'lines',
-                             line = {'color': color,
-                                     'width': .5},
-                             stackgroup = 'one',
-                             hovertemplate = '%{y:.0%}'))
-    
-fig.update_layout(template = 'plotly_white',
-                  title = {'text': '<b>Mode Split for Daily Entries into the Manhattan Hub</b>',
-                           'font_size': 20,
-                           'x': 0.5,
-                           'xanchor':'center',
-                           'y': 0.95,
-                           'yanchor': 'top'},
-                  legend = {'orientation': 'h',
-                            'traceorder': 'normal',
-                            'title_text': '',
-                            'font_size': 16,
-                            'x': 0.5,
-                            'xanchor': 'center',
-                            'y': 1,
-                            'yanchor': 'bottom'},
-                  margin = {'b': 120,
-                            'l': 80,
-                            'r': 80,
-                            't': 120},
-                  xaxis = {'title': {'text': '<b>Year</b>',
-                                     'font_size': 14},
-                           'tickfont_size': 12,
-                           'dtick': 'M12',
-                           'range': [min(df_pct['Year'])-0.5, max(df_pct['Year'])+0.5],
-                           'fixedrange': True,
-                           'showgrid': False},
-                  yaxis = {'title':{'text': '<b>Percent of Total Daily Entries</b>',
-                                    'font_size': 14},
-                           'tickformat': ',.0%',
-                           'tickfont_size': 12,
-                           'rangemode': 'nonnegative',
-                           'fixedrange': True,
-                           'showgrid': True},
-                  font = {'family': 'Arial',
-                          'color': 'black'},
-                  dragmode = False,
-                  hovermode = 'x unified',
-                  hoverlabel = {'font_size': 14})
-
-fig.add_annotation(text = 'Data Source: <a href="https://www.nymtc.org/Data-and-Modeling/Transportation-Data-and-Statistics/Publications/Hub-Bound-Travel" target="blank">NYMTC Hub Bound Travel </a> | <a href="https://raw.githubusercontent.com/NYCPlanning/td-trends/main/hubbound/all_modes/hubbound_all.csv" target="blank">Download Chart Data</a>',
-                    font_size = 14,
-                    showarrow = False,
-                    x = 1,
-                    xanchor = 'right',
-                    xref = 'paper',
-                    y = -0.1,
-                    yanchor = 'top',
-                    yref = 'paper')
-fig
-
-# fig.write_html(path + 'annotations/mode_split.html',
-#               include_plotlyjs='cdn',
-#               config={'displayModeBar':False})
-
-print('Chart Available at: https://nycplanning.github.io/td-trends/hubbound/all_modes/annotations/mode_split.html')   
+print('Chart Available at: https://nycplanning.github.io/td-trends/hubbound/all_modes/annotations/entries.html')   
