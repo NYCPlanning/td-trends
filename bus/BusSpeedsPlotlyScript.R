d6487e1e1dd08bb6f6e1dce026fb08dc50b33fd0
ssanichar <93738034+ssanichar@users.noreply.github.com>
2022-02-07 11:25:52 -0500

    update

--- bus/BusSpeedsPlotlyScript.R
+++ bus/BusSpeedsPlotlyScript.R
@@ -23,27 +23,27 @@ p=p %>%
             x=df[["Month"]], y=df[["Bronx"]], 
             name = "Bronx", 
             color = "#729ece",
-            hovertemplate="%[y:,.0f") %>%
+            hovertemplate="%{y:,.0f}") %>%
   add_trace(type="scatter", 
             mode="lines", 
             x=df[["Month"]], y=df[["Manhattan"]], 
             name = "Manhattan",
             color = "#67bf5c",
-            hovertemplate="%[y:,.0f") %>%
+            hovertemplate="%{y:,.0f}") %>%
   add_trace(type="scatter", 
             mode="lines", 
             x=df[["Month"]], y=df[["Queens"]], 
             name = "Queens", 
             color = "#ad8bc9",
-            hovertemplate="%[y:,.0f") %>%
+            hovertemplate="%{y:,.0f}") %>%
   add_trace(type="scatter", 
             mode="lines", 
             x=df[["Month"]], y=df[["Staten.Island"]], 
             name = 'Staten Island', 
             color = "#ed665d",
-            hovertemplate="%[y:,.0f") %>%
+            hovertemplate="%{y:,.0f}") %>%
   layout(title = "<b>Bus Speeds in 2020-2021</b>", 
-         xaxis=list(title="<b>Month</b>"), yaxis=list(title="<b>Speed (mph)</b>")) %>%
+         xaxis=list(title="<b>Month/Year</b>"), yaxis=list(title="<b>Speed (mph)</b>")) %>%
   layout(margin = list(b=160), 
          annotations=list(x=1, y=-0.3, 
                           text= "Data Source: <a href='http://busdashboard.mta.info/' target='blank'>MTA</a> | <a href='https://raw.githubusercontent.com/NYCPlanning/td-trends/main/bus/BusSpeedAveragesbyBoroughUpdated.csv' target='blank'>Download Chart Data</a>", 
@@ -59,5 +59,3 @@ p
 path = "C:/Users/S_Sanich/Desktop/td-trends/bus"
 
 htmlwidgets::saveWidget(p,paste0(path,"bus_speeds.html"))
-
-
