2ba0ba920c7141c1dfe7be68a853806655376299
ssanichar <93738034+ssanichar@users.noreply.github.com>
2022-02-07 15:05:43 -0500

    update

--- cbd/Bridge_And_Tunnel_Script.R
+++ cbd/Bridge_And_Tunnel_Script.R
@@ -20,49 +20,49 @@ p=p %>%
             mode="lines",
             x=df[["Year"]],
             y=df[["George_Washington_Bridge"]],
-            line = list(color = "#6dccda", width = 4),
-name = "George Washington Bridge",
-hovertemplate = "%{y:,.0f}") %>%
+            line = list(color = "#6dccda", width = 2),
+            name = "George Washington Bridge",
+            hovertemplate = "%{y:,.0f}") %>%
   add_trace(type="scatter",
             mode="lines",
             x=df[["Year"]],
             y=df[["Lincoln_Tunnel"]],
-            line = list(color = "#cdcc5d", width = 4),
+            line = list(color = "#cdcc5d", width = 2),
             name = "Lincoln Tunnel",
             hovertemplate = "%{y:,.0f}") %>%
   add_trace(type="scatter", 
             mode="lines",
             x=df[["Year"]],
             y=df[["Holland_Tunnel"]],
-            line = list(color = "#a2a2a2", width = 4),
-            name = "7AM-7PM ERB Inbound",
+            line = list(color = "#a2a2a2", width = 2),
+            name = "HOlland Tunnel",
             hovertemplate ="%{y:,.0f}") %>%
   add_trace(type="scatter",
             mode = "lines",
             x=df[["Year"]],
             y=df[["Goethals_Bridge"]],
-            line=list(color="#ed97ca", width=4),
+            line=list(color="#ed97ca", width=2),
             name="Goethals Bridge",
             hovertemplate ="%{y:,.0f}") %>%
   add_trace(type="scatter",
             mode = "lines",
             x=df[["Year"]],
             y=df[["Outerbridge_Crossing"]],
-            line=list(color="#a8786e", width=4),
+            line=list(color="#a8786e", width=2),
             name="Outerbridge Crossing",
             hovertemplate ="%{y:,.0f}") %>%
   add_trace(type="scatter",
             mode = "lines",
             x=df[["Year"]],
             y=df[["Bayonne_Bridge"]],
-            line=list(color="#ad8bc9", width=4),
+            line=list(color="#ad8bc9", width=2),
             name="Bayonne Bridge",
             hovertemplate ="%{y:,.0f}") %>%
   layout(title = "<b>Bridge and Tunnel Crossings</b>",
          xaxis = list(title="<b>Year</b>"),
          yaxis = list(title="<b>Number of Crossings</b>")) %>%
   config(displayModeBar=F) %>%
-  layout(annotations=list(text='Data Source: <a href ="https://www.panynj.gov/bridges-tunnels/en/traffic---volume-information---b-t.html" target = "blank">The Port Authority of NY & NJ</a>', 
+  layout(annotations=list(text='Data Source: <a href ="https://www.panynj.gov/bridges-tunnels/en/traffic---volume-information---b-t.html" target = "blank">The Port Authority of NY & NJ</a> | <a href ="https://raw.githubusercontent.com/NYCPlanning/td-trends/main/cbd/Bridge_Tunnel_Crossings.csvhttps://raw.githubusercontent.com/NYCPlanning/td-trends/main/cbd/CBDMN2019_2021.csv" target="blank">Download Chart Data</a>', 
                           font=list(size=11),
                           showarrow=F,
                           x=1,
@@ -73,6 +73,7 @@ hovertemplate = "%{y:,.0f}") %>%
                           yref='paper')) %>%
   
   layout(margin = list(b=160)) %>%
+  layout(xaxis=list(showgrid=FALSE)) %>%
   config(displayModeBar=F)
 
 
