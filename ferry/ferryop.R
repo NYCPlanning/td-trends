a9c24de4e6db7b512036288b21f41dc5108479f0
Yijun Ma <mayijun1203@gmail.com>
2022-03-03 23:42:59 -0500

    update

--- ferry/ferryop.R
+++ ferry/ferryop.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 library(plotly)
 
-path='C:/Users/Y_Ma2/Desktop/GITHUB/td-trends/'
-# path='C:/Users/mayij/Desktop/DOC/GITHUB/td-trends/'
+# path='C:/Users/Y_Ma2/Desktop/GITHUB/td-trends/'
+path='C:/Users/mayij/Desktop/DOC/GITHUB/td-trends/'
 
 df=read.csv(paste0(path,'ferry/ferryop.csv'),stringsAsFactors=F,check.names=F)
 df=df %>%
@@ -25,21 +25,30 @@ for (i in c('Staten Island Ferry','BillyBey','Liberty Landing Ferry','NY Waterwa
               mode='none',
               x=df[['Date']],
               y=df[[i]],
+              showlegend=T,
               stackgroup='one',
               groupnorm='',
               orientation='v',
               fill='tonexty',
               fillcolor=dfcolors[i],
-              hovertemplate='%{y:,.0f}',
-              xhoverformat='<b>%b %Y</b>')
+              hovertext=paste0('<b>',i,': </b>',format(df[[i]],trim=T,big.mark=',')),
+              hoverinfo='text')
 }
+p=p %>%
+  add_trace(type='scatter',
+            mode='none',
+            x=df[['Date']],
+            y=df[['Staten Island Ferry']],
+            showlegend=F,
+            hovertext=paste0('<b>Month: </b>',format(df[['Date']],'%b %Y')),
+            hoverinfo='text')
 p=p %>%
   layout(template='plotly_white',
          title=list(text=paste0('<b>Ferry Ridership by Operator</b>'),
                     font=list(size=20),
                     x=0.5,
                     xanchor='center',
-                    y=0.95,
+                    y=0.98,
                     yanchor='top'),
          legend=list(orientation='h',
                      title=list(text=''),
@@ -50,39 +59,41 @@ p=p %>%
                      yanchor='bottom'),
          margin=list(b=120,
                      l=80,
-                     r=80,
+                     r=40,
                      t=120),
          xaxis=list(title=list(text='<b>Month</b>',
                                font=list(size=14)),
                     tickfont=list(size=12),
                     tickformat='%b %Y',
                     dtick='M6',
-                    range=c(min(df[['Date']])-60,max(df[['Date']])+60),
+                    range=c(min(df[['Date']])-15,max(df[['Date']])+15),
                     fixedrange=T,
                     showgrid=F),
          yaxis=list(title=list(text='<b>Ridership</b>',
                                font=list(size=14)),
                     tickfont=list(size=12),
-                    rangemode='nonnegative',
+                    rangemode='tozero',
                     fixedrange=T,
                     showgrid=T,
                     zeroline=T,
-                    zerolinecolor='#444',
-                    zerolinewidth=1),
+                    zerolinecolor='rgba(0,0,0,0.2)',
+                    zerolinewidth=2),
          hoverlabel=list(font=list(size=14)),
          font=list(family='Arial',
                    color='black'),
          dragmode=F,
-         hovermode='x unified',
-         annotations=list(text='Data Source: <a href="https://www1.nyc.gov/html/dot/html/about/datafeeds.shtml#ferry" target="blank">NYC DOT</a> | <a href="https://raw.githubusercontent.com/NYCPlanning/td-trends/main/ferry/ferryop.csv" target="blank">Download Chart Data</a>',
-                          font=list(size=14),
-                          showarrow=F,
-                          x=1,
-                          xanchor='right',
-                          xref='paper',
-                          y=-0.1,
-                          yanchor='top',
-                          yref='paper'))
+         hovermode='x unified')
+p=p %>% 
+  add_annotations(text='Data Source: <a href="https://www1.nyc.gov/html/dot/html/about/datafeeds.shtml#ferry" target="blank">NYC DOT</a> | <a href="https://raw.githubusercontent.com/NYCPlanning/td-trends/main/ferry/ferryop.csv" target="blank">Download Chart Data</a>',
+                  font=list(size=14),
+                  showarrow=F,
+                  x=1,
+                  xanchor='right',
+                  xref='paper',
+                  y=0,
+                  yanchor='top',
+                  yref='paper',
+                  yshift=-80)
 p=p %>%
   config(displayModeBar=F)
 p
