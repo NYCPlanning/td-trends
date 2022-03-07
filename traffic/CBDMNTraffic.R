49b973fadcc792dc098d8474901b3d284351cfbb
Yijun Ma <mayijun1203@gmail.com>
2022-03-06 21:50:21 -0500

    update

--- traffic/CBDMNTraffic.R
+++ traffic/CBDMNTraffic.R
@@ -7,8 +7,12 @@ path='C:/Users/mayij/Desktop/DOC/GITHUB/td-trends/'
 df=read.csv(paste0(path,'traffic/CBDMN2019_2021.csv'),stringsAsFactors=F,check.names=F)
 df=df %>%
   mutate(Date=as.Date(Date,'%m/%d/%Y')) %>%
-  select(Date,TBTA_CBDInbound_Avg,PANYNJ_CBDInbound_Avg,ERB_Inbound_Avg,TPEP_CBD_Speed_Avg)
-colnames(df)=c('Date','MTA Bridges & Tunnels','PANYNJ Bridges & Tunnels','East River Bridges','Taxi Speed')
+  select(Date,TBTA_CBDInbound_Avg,PANYNJ_CBDInbound_Avg,ERB_Inbound_Avg,
+         Total_CBDInbound_Volume,TBTA_CBDInbound_Pct,PANYNJ_CBDInbound_Pct,
+         ERB_Inbound_Pct,TPEP_CBD_Speed_Avg)
+colnames(df)=c('Date','MTA Bridges & Tunnels','PANYNJ Bridges & Tunnels',
+               'East River Bridges','Total Inbound Volume','MTA Bridges & Tunnels Pct',
+               'PANYNJ Bridges & Tunnels Pct','East River Bridges Pct','Taxi Speed')
 
 dfcolors=c('MTA Bridges & Tunnels'='rgba(114,158,206,0.6)',
            'PANYNJ Bridges & Tunnels'='rgba(109,204,218,0.6)',
@@ -28,6 +32,14 @@ p=p %>%
             showlegend=T,
             hovertext=paste0('<b>Taxi Speed: </b>',format(round(df[['Taxi Speed']],2),trim=T,nsmall=2)),
             hoverinfo='text')
+p=p %>%
+  add_trace(type='scatter',
+            mode='none',
+            x=df[['Date']],
+            y=df[['MTA Bridges & Tunnels']],
+            showlegend=F,
+            hovertext=paste0('<b>Total Inbound Volume: </b>',format(df[['Total Inbound Volume']],trim=T,big.mark=',')),
+            hoverinfo='text')
 for (i in c('East River Bridges','MTA Bridges & Tunnels','PANYNJ Bridges & Tunnels')){
   p=p %>%
     add_trace(name=i,
@@ -41,7 +53,8 @@ for (i in c('East River Bridges','MTA Bridges & Tunnels','PANYNJ Bridges & Tunne
               orientation='v',
               fill='tonexty',
               fillcolor=dfcolors[i],
-              hovertext=paste0('<b>',i,': </b>',format(df[[i]],trim=T,big.mark=',')),
+              hovertext=paste0('<b>',i,': </b>',format(df[[i]],trim=T,big.mark=','),
+                               ' (',format(round(df[[paste0(i,' Pct')]]*100,0),trim=T,nsmall=0),'%)'),
               hoverinfo='text')
 }
 p=p %>%
@@ -54,7 +67,7 @@ p=p %>%
             hoverinfo='text')
 p=p %>%
   layout(template='plotly_white',
-         title=list(text=paste0('<b>Manhattan CBD Traffic Volumn and Speed <br>(7-Day Moving Average)</b>'),
+         title=list(text=paste0('<b>Manhattan CBD Daily Traffic Volumn and Speed <br>(7-Day Moving Average)</b>'),
                     font=list(size=20),
                     x=0.5,
                     xanchor='center',
